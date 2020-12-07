7460254be62cfdee321f8bdc13916ec4775633b5
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2020-12-06 19:34:08 -0500

    Added rustfmt setup and a license (subject to change)

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate lazy_static;
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate rocket;
 
-use std::sync::Mutex;
 use sqlite;
+use std::sync::Mutex;
 
 lazy_static! {
-    static ref CONN: Mutex<sqlite::Connection> = {
-        Mutex::new(sqlite::open("./db.sqlite").unwrap())
-    };
+	static ref CONN: Mutex<sqlite::Connection> =
+		{ Mutex::new(sqlite::open("./db.sqlite").unwrap()) };
 }
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+	"Hello, world!"
 }
 
 fn main() {
-    CONN.lock().unwrap();
-    rocket::ignite().mount("/", routes![index]).launch();
+	CONN.lock().unwrap();
+	rocket::ignite().mount("/", routes![index]).launch();
 }
